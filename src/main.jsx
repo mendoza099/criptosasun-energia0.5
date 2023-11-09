a745aacfc9d75c6b1e35fe9b6ba9ef0ca47eb9f7
Ehiber Graterol <56272634+ehiber@users.noreply.github.com>
2023-11-09 17:13:03 +0000

    :bug: fix bootstrap bug

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Layout from './Layout.jsx'
 import './index.css'
-import * as bootstrap from 'bootstrap'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
