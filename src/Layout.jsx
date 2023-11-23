85f1922b47148d6f2bbf62838911f2fd83febbcb
Ehiber Graterol <56272634+ehiber@users.noreply.github.com>
2023-11-23 16:51:06 +0000

    :sparkles: added final routes config

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,43 +1,9 @@
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import { Root } from "./pages/Root";
-import { Home } from "./pages/Home";
-import { Single } from "./pages/Single";
+import {  RouterProvider } from "react-router-dom";
 import injectContext from "./context/AppContext";
-import { Demo } from "./pages/Demo";
+import { router } from "./routes";
 
-const router = createBrowserRouter([
-    {
-        path: "/",
-        element: (
-            <Root/>
-        ),
-        children: [
-            {
-                path: "/",
-                element: <Home />,
-            },
-            {
-                path: "single/:theId",
-                element: <Single />,
-            },
-            {
-                path: "demo",
-                element: <Demo />,
-            },
-        ],
-        errorElement: <h1>Not found!</h1>,
-    }
-  ]);
-
-//create your first component
+//This component contains your entire application
 const Layout = () => {
-    //the basename is used when your project is published in a subdirectory and not in the root of the domain
-    // you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
-    // const basename = process.env.BASENAME || "";
-
     return (
         <RouterProvider router={router} />
     );
---
+++ src/routes.jsx
@@ -0,0 +1,24 @@
+import {
+    createBrowserRouter,
+    createRoutesFromElements,
+    Route,
+} from "react-router-dom";
+import { BaseHome } from "./pages/BaseHome";
+import { Home } from "./pages/Home";
+import { Single } from "./pages/Single";
+import { Demo } from "./pages/Demo";
+
+export const router = createBrowserRouter(
+    createRoutesFromElements(
+    // Create your routes here, if you want to keep the Navbar and Footer in all views, add your new routes inside the containing Route.
+    // Root, on the contrary, create a sister Route, if you have doubts, try it!
+    // Note: keep in mind that errorElement will be the default page when you don't get a route, customize that page to make your project more attractive.
+    // Note: The child paths of the BaseHome element replace the Outlet component with the elements contained in the "element" attribute of these child paths.
+
+      <Route path="/" element={<BaseHome />} errorElement={<h1>Not found!</h1>} >
+        <Route path="single/:theId" element={ <Single />} />
+        <Route path="demo" element={<Demo />} />
+        <Route path= "/" element={<Home />} />
+      </Route>
+    )
+);
