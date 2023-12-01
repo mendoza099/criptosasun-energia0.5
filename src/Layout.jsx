559f011d22f07e2b4b9b5ec7456fd24194d16fb1
Ehiber Graterol <56272634+ehiber@users.noreply.github.com>
2023-12-01 10:00:11 +0000

    :bulb: add comment in Layout

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,10 +1,11 @@
 import {  RouterProvider } from "react-router-dom";
 import Context from "./context/_AppContext";
 import injectContext from "./context/AppContext";
+// This is where all your routes are located
 import { router } from "./routes";
 import { useStore } from "./hooks/useStore";
 
-//This component contains your entire application
+// This component contains your entire application
 const Layout = () => {
     const store = useStore();
 
